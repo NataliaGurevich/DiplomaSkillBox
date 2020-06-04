7f30334a92973f4291768b33da476e944930d369
Natalia Gurevich <gurevich.natalia@gmail.com>
2020-06-04 10:39:21 +0200

    add schedule to del captcha

--- src/main/java/com/skillbox/diploma/DiplomaSkillBox/main/repository/CaptchaRepository.java
+++ src/main/java/com/skillbox/diploma/DiplomaSkillBox/main/repository/CaptchaRepository.java
@@ -4,6 +4,9 @@ import com.skillbox.diploma.DiplomaSkillBox.main.model.CaptchaCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.Instant;
+import java.util.List;
+
 public interface CaptchaRepository extends JpaRepository<CaptchaCode, Long> {
 
     @Query(value = "SELECT secret_code FROM captcha_codes WHERE code=?1", nativeQuery = true)
@@ -13,4 +16,7 @@ public interface CaptchaRepository extends JpaRepository<CaptchaCode, Long> {
 
     @Query(value = "SELECT MAX(id) FROM captcha_codes", nativeQuery = true)
     Long getLastId();
+
+    @Query(value = "SELECT * FROM captcha_codes WHERE time < ?1", nativeQuery = true)
+    List<CaptchaCode> getCaptchaCodeLastHour(Instant time);
 }
